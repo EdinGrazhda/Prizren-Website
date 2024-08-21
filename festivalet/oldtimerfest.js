ac76efe5cdfb397706bb3956c0d1dd95afecaca4
Edin <edin@stree.agency>
2024-08-22 00:31:56 +0200

    Prizren Code Fest website for application

---
+++ festivalet/oldtimerfest.js
@@ -0,0 +1,212 @@
+
+var i = 0;
+var price = 30;
+var quantity = 0;
+
+function incNumber() {
+    if (i < 20) {
+        i++;
+    } else if (i = 20) {
+        i = 0;
+    }
+    document.getElementById("display").innerHTML = i;
+
+    quantity++;
+    var totalPrice = price * quantity;
+    document.getElementById("price").innerHTML = "Rent Price: " + totalPrice + " Euros";
+
+    
+}
+
+function decNumber() {
+    if (i > 0) {
+        --i;
+    } else if (i = 0) {
+        i = 10;
+    }
+    document.getElementById("display").innerHTML = i;
+
+    if (quantity > 0) {
+        quantity--;
+        var totalPrice = price * quantity;
+        document.getElementById("price").innerHTML = "Rent Price: " + totalPrice + " Euros";
+}
+}
+
+var i = 0;
+var price = 40;
+var quantity = 0;
+
+function incNumber1() {
+    if (i < 10) {
+        i++;
+    } else if (i = 10) {
+        i = 0;
+    }
+    document.getElementById("display1").innerHTML = i;
+
+    quantity++;
+    var totalPrice = price * quantity;
+    document.getElementById("price1").innerHTML = "Rent Price: " + totalPrice + " Euros";
+}
+
+function decNumber1() {
+    if (i > 0) {
+        --i;
+    } else if (i = 0) {
+        i = 10;
+    }
+    document.getElementById("display1").innerHTML = i;
+    if (quantity > 0) {
+        quantity--;
+        var totalPrice = price * quantity;
+        document.getElementById("price1").innerHTML = "Rent Price: " + totalPrice + " Euros";
+
+}
+}
+
+var i = 0;
+var price =  50;
+var quantity = 0;
+
+function incNumber2() {
+    if (i < 10) {
+        i++;
+    } else if (i = 10) {
+        i = 0;
+    }
+    document.getElementById("display2").innerHTML = i;
+
+    quantity++;
+    var totalPrice = price * quantity;
+    document.getElementById("price2").innerHTML = "Rent Price: " + totalPrice + " Euros";
+}
+
+function decNumber2() {
+    if (i > 0) {
+        --i;
+    } else if (i = 0) {
+        i = 10;
+    }
+    document.getElementById("display2").innerHTML = i;
+
+    document.getElementById("display1").innerHTML = i;
+    if (quantity > 0) {
+        quantity--;
+        var totalPrice = price * quantity;
+        document.getElementById("price2").innerHTML = "Rent Price: " + totalPrice + " Euros";
+
+}
+}
+
+var i = 0;
+var price = 80445 ;
+var quantity = 0;
+
+function incNumber3() {
+    if (i < 10) {
+        i++;
+    } else if (i = 10) {
+        i = 0;
+    }
+    document.getElementById("display3").innerHTML = i;
+
+    quantity++;
+    var totalPrice = price * quantity;
+    document.getElementById("price3").innerHTML = "Rent Price: " + totalPrice + " Euros";
+}
+
+function decNumber3() {
+    if (i > 0) {
+        --i;
+    } else if (i = 0) {
+        i = 10;
+    }
+    document.getElementById("display3").innerHTML = i;
+
+    
+    if (quantity > 0) {
+        quantity--;
+        var totalPrice = price * quantity;
+        document.getElementById("price3").innerHTML = "Rent Price: " + totalPrice + " Euros";
+
+}
+}
+var i = 0;
+var price = 20;
+var quantity = 0;
+
+function incNumber4() {
+    if (i < 10) {
+        i++;
+    } else if (i = 10) {
+        i = 0;
+    }
+    document.getElementById("display4").innerHTML = i;
+
+    quantity++;
+    var totalPrice = price * quantity;
+    document.getElementById("price4").innerHTML = "Rent Price: " + totalPrice + " Euros";
+}
+
+function decNumber4() {
+    if (i > 0) {
+        --i;
+    } else if (i = 0) {
+        i = 10;
+    }
+    document.getElementById("display4").innerHTML = i;
+
+    document.getElementById("display4").innerHTML = i;
+    if (quantity > 0) {
+        quantity--;
+        var totalPrice = price * quantity;
+        document.getElementById("price4").innerHTML = "Total Price: " + totalPrice + " Euros";
+
+}
+}
+var i = 0;
+var price = 100;
+var quantity = 0;
+
+function incNumber5() {
+    if (i < 10 ) {
+        i++;
+        
+
+        
+    } else if (i = 10) {
+        i = 0;
+    }
+    document.getElementById("display5").innerHTML = i;
+
+    quantity++;
+    var totalPrice = price * quantity;
+    document.getElementById("price5").innerHTML = "Rent Price: " + totalPrice + " Euros";
+
+    
+}
+
+function decNumber5() {
+    if (i > 0) {
+        --i;
+    } else if (i = 0) {
+        i = 10;
+    }
+    document.getElementById("display5").innerHTML = i;
+
+    document.getElementById("display1").innerHTML = i;
+    if (quantity > 0) {
+        quantity--;
+        var totalPrice = price * quantity;
+        document.getElementById("price5").innerHTML = "Rent Price: " + totalPrice + " Euros";
+
+}
+}
+
+
+
+
+
+
+
